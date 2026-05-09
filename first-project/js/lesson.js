46715fc7fd3a0464a756b508a0368d275e2bea17
yusaku <s.yusaku1220@gmail.com>
2026-05-10 04:41:59 +0900

    アロー関数

--- first-project/js/lesson.js
+++ first-project/js/lesson.js
@@ -1,44 +1,71 @@
-const ojb = [{
-  name: "shima",
-  age: 23,
-}];
-console.log(ojb.name);
-const age = "80";
-const is = age >= 18 ? "成人" : "bubui";
-console.log(is);
+// const ojb = [{
+//   name: "shima",
+//   age: 23,
+// }];
+// console.log(ojb.name);
+// const age = "80";
+// const is = age >= 18 ? "成人" : "bubui";
+// console.log(is);
 
-intNum = parseInt(age);
-console.log(intNum);
+// intNum = parseInt(age);
+// console.log(intNum);
 
 
-let num1 = 10;
-let num2 = 5;
+// let num1 = 10;
+// let num2 = 5;
 
-let isEqual = num1 === num2
-console.log(isEqual);
+// let isEqual = num1 === num2
+// console.log(isEqual);
 
-let day = "月";
+// let day = "月";
 
-switch (day) {
-    case "月":
-        console.log(day);
-        break;
-    case "火":
-        console.log("火曜日");
-        break;
-    default:
-        console.log("終わり");
+// switch (day) {
+//     case "月":
+//         console.log(day);
+//         break;
+//     case "火":
+//         console.log("火曜日");
+//         break;
+//     default:
+//         console.log("終わり");
+// }
+
+
+// const fruits = ["りんご","みかん","キウイ"]
+// for (const fruit of fruits){
+//     console.log(fruit);
+// }
+
+
+// console.log(ojb.name)
+
+// for (let person of ojb) {
+//     console.log(person.name);
+// }
+
+
+function add (a,b) {
+    return a + b;
+}
+console.log(add(3,4));
+console.log(add);
+
+
+const sampleFunc = function () {
+    console.log("無名関数");
 }
 
+sampleFunc();
 
-const fruits = ["りんご","みかん","キウイ"]
-for (const fruit of fruits){
-    console.log(fruit);
+const cl= (a) => {
+    console.log(a);
 }
 
+console.log(cl);
 
-console.log(ojb.name)
+const addHundred = (a) => a+100;
+console.log(addHundred(90));
 
-for (let person of ojb) {
-    console.log(person.name);
-}
+const mag = prompt()
+
+
