2b9dff430364df8588637dc3061b81478a8d65fb
yusaku <s.yusaku1220@gmail.com>
2026-05-11 00:52:05 +0900

    重複修正 リファクタリング

--- first-project/js/lesson.js
+++ first-project/js/lesson.js
@@ -1,47 +1,68 @@
 let todoList = [];
 
+// 入力された新しいTODOの値を取得し、todoList変数に代入する関数
+const registerNewTodo = () => {
+  // TODOの値を取得する
+  const newTodo = document.getElementById("new-todo-name");
+  const newPerson = document.getElementById("new-person");
+  const newDeadline = document.getElementById("deadline");
 
-document.addEventListener("DOMContentLoaded",() => {
-    const registerButtonElem = document.getElementById("register");
-    registerButtonElem.addEventListener("click", () =>{
-        // TODOの値を取得する
-        const newTodo = document.getElementById("new-todo-name");
-        const newPerson = document.getElementById("new-person");
-        const newDeadline = document.getElementById("deadline");
+  // 入力された値を配列に追加
+  todoList.push({
+    todoName: newTodo.value,
+    person: newPerson.value,
+    deadline: newDeadline.value,
+  });
+};
 
-        console.log(newTodo.value);
-        console.log(newPerson.value);
-        console.log(newDeadline.value);
+// TODO一覧を全て削除する関数
+const removeTodoElem = () => {
+  const tbody = document.getElementById("todo-data");
 
-        const newTodoObject = {
-            todoName:newTodo.value,
-            person:newPerson.value,
-            deadline:newDeadline.value
-        };
-        
-        todoList.push(newTodoObject);
+  while (tbody.firstChild) {
+    tbody.firstChild.remove();
+  }
+};
 
-        console.log(todoList);
+// TODO一覧を表示する関数
+const TodoListDisplay = () => {
+  todoList.forEach((todo) => {
+    const tbody = document.getElementById("todo-data");
+    
+    const trElem = document.createElement("tr");
 
-        todoList.forEach((todo) => {
-            const trElem = document.createElement("tr");
+    const todoNameTdElem = document.createElement("td");
+    todoNameTdElem.textContent = todo.todoName;
 
-            const todoNameTdElem = document.createElement("td");
-            todoNameTdElem.textContent = todo.todoName
+    const personTdElem = document.createElement("td");
+    personTdElem.textContent = todo.person;
 
-            const personTdElem = document.createElement("td");
-            personTdElem.textContent = todo.person
+    const deadlineTdElem = document.createElement("td");
+    deadlineTdElem.textContent = todo.deadline;
 
-            const deadlineTdElem = document.createElement("td");
-            deadlineTdElem.textContent = todo.deadline
+    trElem.appendChild(todoNameTdElem);
+    trElem.appendChild(personTdElem);
+    trElem.appendChild(deadlineTdElem);
 
-            trElem.appendChild(todoNameTdElem);
-            trElem.appendChild(personTdElem);
-            trElem.appendChild(deadlineTdElem);
+    tbody.appendChild(trElem);
+  });
+};
 
-            const tbody = document.getElementById("todo-data");
-            tbody.appendChild(trElem);
 
-        })
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  const registerButtonElem = document.getElementById("register");
+  registerButtonElem.addEventListener("click", () => {
+    // TODOの値を取得する
+    registerNewTodo();
+
+    //todo一覧を削除
+    removeTodoElem();
+    // todo一覧を表示
+    TodoListDisplay();
+  });
 });
+
+
+
+
+
